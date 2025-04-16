274a249be58bbc5a7e76628e30cff4b6936b589d
Totoo707 <thomaslozancic@gmail.com>
2025-04-16 10:25:32 +0200

    correction projectManager

--- projectManager.js
+++ projectManager.js
@@ -1,21 +1,32 @@
 function createProjectManager() {
     const projects = [];
 
+    function cloneProject(project) {
+        return { 
+            ...project, 
+            tasks: project.tasks.map(task => ({ ...task })) 
+        };
+    }
+
     return {
         addProject(project) {
-            projects.push(project);
+            const clonedProject = { 
+                ...project, 
+                tasks: project.tasks ? project.tasks.map(task => ({ ...task })) : [] 
+            };
+            projects.push(clonedProject);
         },
 
         addTaskToProject(projectId, task) {
             const project = projects.find(p => p.id === projectId);
             if (project) {
-                project.tasks.push(task);
+                project.tasks.push({ ...task });
             }
         },
 
         getProjectById(projectId) {
             const project = projects.find(p => p.id === projectId);
-            return project;
+            return project ? cloneProject(project) : undefined;
         },
 
         markTaskAsCompleted(projectId, taskId) {
@@ -30,4 +41,4 @@ function createProjectManager() {
     };
 }
 
-module.exports = createProjectManager;  // Exportez la fonction
+module.exports = createProjectManager;
